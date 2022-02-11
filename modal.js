04aa73c039c3f48f80475713dce9f220daa06218
Terry Sutton <saltcod@gmail.com>
2022-02-11 15:31:31 -0330

    Add dom ready

--- modal.js
+++ modal.js
@@ -1,10 +1,13 @@
-
-const selects = document.querySelectorAll('.mktoFormRow select');
-console.log(selects);
-selects.forEach(select => {
-	select.addEventListener('change', () => {
-		setTimeout(() => {
-			window.parent.postMessage(document.body.scrollHeight, '*');
-		}, 200);
-	})
-})
+document.addEventListener("DOMContentLoaded", function() {
+	const selects = document.querySelectorAll('.mktoFormRow select');
+	if ( selects.length < 0 ) {
+		return;
+	}
+	selects.forEach(select => {
+		select.addEventListener('change', () => {
+			setTimeout(() => {
+				window.parent.postMessage(document.body.scrollHeight, '*');
+			}, 200);
+		})
+	});
+});

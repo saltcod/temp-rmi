658c0511bd77df0635d25628e605733ec727718d
Terry Sutton <saltcod@gmail.com>
2022-02-11 12:17:48 -0330

    Add onchange to select

--- modal.js
+++ modal.js
@@ -1,5 +1,10 @@
-document.addEventListener('click', () => {
-	setTimeout(() => {
-		window.parent.postMessage(document.body.scrollHeight, '*');
-	}, 200);
+
+const selects = document.querySelectorAll('.mktoFormRow select');
+
+selects.forEach(select => {
+	select.addEventListener('onChange', () => {
+		setTimeout(() => {
+			window.parent.postMessage(document.body.scrollHeight, '*');
+		}, 200);
+	})
 })

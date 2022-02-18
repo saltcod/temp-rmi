12151271c79d8552bdda8cf30cf5f7922fdd2986
Terry Sutton <saltcod@gmail.com>
2022-02-18 15:23:24 -0330

    Change styles

--- modal.js
+++ modal.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 	console.log('loaded');
 	const selects = document.querySelectorAll('.mktoFormRow select');
+
 	if ( selects.length < 0 ) {
 		return;
 	}
+	console.log('after if');
 
 	selects.forEach(select => {
 		select.addEventListener('change', () => {
